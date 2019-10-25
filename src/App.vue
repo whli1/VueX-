5f3a9c48fa829477eeede22f1f8b0ffac72b3add
whli <whl802302@outlook.com>
2019-10-25 17:59:56 +0800

    '清除定时器'

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 
 <script>
   import store from './store'
+
   export default {
     name: 'App',
     store,//所有的组件对象都多了一个属性：$store
@@ -21,19 +22,22 @@
     },
     methods: {
       add() {
-      // 通知vuex去增加
+        // 通知vuex去增加
+        clearInterval(this.timeOut);
         this.$store.dispatch('add');
       },
       reduce() {
+        clearInterval(this.timeOut);
         this.$store.dispatch('reduce');
       },
       oddAdd() {
-        if ( this.$store.state.count % 2 !== 0) {
+        clearInterval(this.timeOut);
+        if (this.$store.state.count % 2 !== 0) {
           this.$store.dispatch('add');
         }
       },
       asyncADD() {
-        setInterval(() => {
+        this.timeOut = setInterval(() => {
           this.$store.dispatch('add');
         }, 1000)
       }
